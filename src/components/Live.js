4d544b363c3dae7c70eb73d5a015ba3840c4d993
Siddharth Singh Kushwaha <siddharthmnbv@gmail.com>
2023-07-16 16:27:56 +0530

    text error handeling

--- src/components/Live.js
+++ src/components/Live.js
@@ -26,18 +26,23 @@ const Live = (props) => {
     "Nov",
     "Dec",
   ]
-console.log(props)
+
   return (
     <div className='d-flex flex-column justify-content-between mt-4 styles1 p-2 align-item-center'>
       <h1 className='text-style2 d-flex justify-content-center'>{props.liveData.temp}°C</h1>
-      <p className='p-style '>{`
-      ${months[dayjs.unix(props.liveData.dt).month()]} 
-      ${dayjs.unix(props.liveData.dt).date()} 
-      ${days[dayjs.unix(props.liveData.dt).day()]}`}
+      <p className='p-style'>
+        {props.liveData === NaN ? 
+          `${months[dayjs.unix(props.liveData.dt).month()]} 
+          ${dayjs.unix(props.liveData.dt).date()} 
+          ${days[dayjs.unix(props.liveData.dt).day()]}` 
+          : ""
+        }
       </p>
       <div className='details d-flex flex-column align-items-center'>
-        <img className='icon-style text-style01' src={`http://openweathermap.org/img/wn/${props.liveData.icon}@2x.png`} alt='Select' />
-        <p className='text-style11'>{props.liveData.type}</p>
+        <img className='icon-style text-style01' src={`http://openweathermap.org/img/wn/${props.liveData.icon}@2x.png`} alt='' />
+        <p className='text-style11'>
+          {props.liveData.type ? props.liveData.type : "Please select a city"}
+       </p>
       </div>
     </div>
   )
